bbb9aeff735b35069248aaf6a9424f583d4d0ae6
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-03-31 10:40:59 -0300

    Back-End - iniciado

---
+++ BackEnd/.env
@@ -0,0 +1,3 @@
+FLASK_APP=main
+FLASK_ENV=development
+FLASK_DEBUG=1
--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add -A
-git commit -m "projeto iniciado"
-git branch -M master
-git remote add origin https://github.com/CarlosViniMSouza/DevPlenoFlaskReactJS.git
-git push -u origin master
+git commit -m "Back-End - iniciado"
+git push
 git status
