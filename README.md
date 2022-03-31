f577253cb687876dddc26f90be167d9b760af1ba
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-03-31 10:13:24 -0300

    projeto iniciado

---
+++ README.md
@@ -0,0 +1,5 @@
+## Processo Seletivo para Desenvolvedor Full-Stack Pleno
+
+```
+Mais detalhes serão adicionados em breve.
+```
---
+++ autogit.sh
@@ -0,0 +1,7 @@
+git init
+git add -A
+git commit -m "projeto iniciado"
+git branch -M master
+git remote add origin https://github.com/CarlosViniMSouza/DevPlenoFlaskReactJS.git
+git push -u origin master
+git status
